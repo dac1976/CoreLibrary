e703b55dd3e26e38e7c1d02e74e7cee2e2a24c33
Duncan Crutchley <duncan.crutchley@gmail.com>
2014-03-08 22:05:29 +0000

    Added another test for DebugLog.

--- UnitTests/DebugLog/tst_DebugLogTest.cpp
+++ UnitTests/DebugLog/tst_DebugLogTest.cpp
@@ -19,6 +19,7 @@ private Q_SLOTS:
     void testCase_DebugLog2();
     void testCase_DebugLog3();
     void testCase_DebugLog4();
+    void testCase_DebugLog5();
 };
 
 DebugLogTest::DebugLogTest()
@@ -157,15 +158,15 @@ void DebugLogTest::testCase_DebugLog4()
 {
     {
         core_lib::log::DebugLog<core_lib::log::DefaultLogFormat, 1024> dl("1.0.0.0", "", "test_log");
-        dl.AddLogMessage("Message 1", __FILE__, __LINE__, core_lib::log::eLogMessageLevel::info);
+        dl.AddLogMessage("Message 1", __FILE__, __LINE__, core_lib::log::eLogMessageLevel::warning);
         dl.AddLogMessage("Message 2", __FILE__, __LINE__, core_lib::log::eLogMessageLevel::info);
-        dl.AddLogMessage("Message 3", __FILE__, __LINE__, core_lib::log::eLogMessageLevel::info);
+        dl.AddLogMessage("Message 3", __FILE__, __LINE__, core_lib::log::eLogMessageLevel::warning);
         dl.AddLogMessage("Message 4", __FILE__, __LINE__, core_lib::log::eLogMessageLevel::info);
-        dl.AddLogMessage("Message 5", __FILE__, __LINE__, core_lib::log::eLogMessageLevel::info);
+        dl.AddLogMessage("Message 5", __FILE__, __LINE__, core_lib::log::eLogMessageLevel::warning);
         dl.AddLogMessage("Message 6", __FILE__, __LINE__, core_lib::log::eLogMessageLevel::info);
-        dl.AddLogMessage("Message 7", __FILE__, __LINE__, core_lib::log::eLogMessageLevel::info);
+        dl.AddLogMessage("Message 7", __FILE__, __LINE__, core_lib::log::eLogMessageLevel::warning);
         dl.AddLogMessage("Message 8", __FILE__, __LINE__, core_lib::log::eLogMessageLevel::info);
-        dl.AddLogMessage("Message 9", __FILE__, __LINE__, core_lib::log::eLogMessageLevel::info);
+        dl.AddLogMessage("Message 9", __FILE__, __LINE__, core_lib::log::eLogMessageLevel::warning);
         dl.AddLogMessage("Message 10", __FILE__, __LINE__, core_lib::log::eLogMessageLevel::info);
     }
 
@@ -176,6 +177,57 @@ void DebugLogTest::testCase_DebugLog4()
     boost::filesystem::remove("test_log_old.txt");
 
     QVERIFY(filesExist);
+}
+
+void DebugLogTest::testCase_DebugLog5()
+{
+    {
+        core_lib::log::DebugLog<core_lib::log::DefaultLogFormat> dl("1.0.0.0", "", "test_log");
+        dl.AddLogMsgLevelFilter(core_lib::log::eLogMessageLevel::warning);
+        dl.AddLogMessage("Message 1", __FILE__, __LINE__, core_lib::log::eLogMessageLevel::warning);
+        dl.AddLogMessage("Message 2", __FILE__, __LINE__, core_lib::log::eLogMessageLevel::info);
+        dl.AddLogMessage("Message 3", __FILE__, __LINE__, core_lib::log::eLogMessageLevel::warning);
+    }
+
+    std::ifstream ifs("test_log.txt");
+    QVERIFY(ifs.is_open());
+    std::string line;
+    size_t lineCount = 0;
+
+    while(!ifs.eof())
+    {
+        std::getline(ifs, line);
+
+        switch(++lineCount)
+        {
+        case 1:
+            QVERIFY(line.substr(36, line.size() - 36) == "< DEBUG LOG STARTED >");
+            break;
+        case 2:
+            QVERIFY(line.substr(36, line.size() - 36) == "< Software Version 1.0.0.0 >");
+            break;
+        case 3:
+            QVERIFY(line.substr(101, line.size() - 101) == "< Message 2 >");
+            break;
+        case 4:
+            QVERIFY(line.substr(36, line.size() - 36) == "< DEBUG LOG STOPPED >");
+            break;
+        case 5:
+            QVERIFY(line == "");
+            break;
+        default:
+            QFAIL("Too many lines");
+        }
+    }
+
+    ifs.close();
+
+    if (lineCount < 5)
+    {
+        QFAIL("Too few lines");
+    }
+
+    boost::filesystem::remove("test_log.txt");
 }
 
 QTEST_APPLESS_MAIN(DebugLogTest)
