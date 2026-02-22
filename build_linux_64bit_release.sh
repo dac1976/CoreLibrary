32b6f22fbcca3b2878214d6d0602113ee6b0ae1c
Duncan Crutchley <drd2k3.coding@tuta.io>
2026-02-22 23:05:27 +0000

    Update build_linux_64bit_release.sh

--- build_linux_64bit_release.sh
+++ build_linux_64bit_release.sh
@@ -7,6 +7,7 @@ set -e
 export CORELIB_ROOT=/home/$USER/Projects/cpp/CoreLibrary
 export CORELIB_BOOST_ROOT=${CORELIB_ROOT}/../ThirdParty/boost_1_90_0
 export CORELIB_BOOST_LIB=${CORELIB_ROOT}/../ThirdParty/boost_1_90_0/lib64-msvc-14.3
+# Not nmeeded fo Linux build
 export CORELIB_BOOST_LIB_NAME_STUB=
 export CORELIB_BOOST_LIB_NAME_STUB_D=
 
@@ -16,12 +17,27 @@ rm -rf build
 rm -rf RelWithDebInfo
 mkdir -p RelWithDebInfo
 
-mkdir -p RelWithDebInfo/bin
+mkdir -p RelWithDebInfo/static/bin
+mkdir -p RelWithDebInfo/dynamic/bin
 
-OUTPUT_DIR="RelWithDebInfo"
+OUTPUT_DIR="RelWithDebInfo/dynamic"
 
 # Create build files
 cmake -Bbuild  -DCMAKE_INSTALL_PREFIX="${OUTPUT_DIR}"  -DBUILD_SHARED_LIBS=ON  -DCORELIB_USE_STD_FILESYSTEM=ON  -DCMAKE_BUILD_TYPE=RelWithDebInfo
+
+# Perform build
+cmake --build build -- -j$(nproc)
+
+# Install
+cmake --install build --prefix "${OUTPUT_DIR}"
+
+# Static build
+rm -rf build
+
+OUTPUT_DIR="RelWithDebInfo/static"
+
+# Create build files
+cmake -Bbuild  -DCMAKE_INSTALL_PREFIX="${OUTPUT_DIR}"  -DBUILD_SHARED_LIBS=OFF  -DCORELIB_USE_STD_FILESYSTEM=ON  -DCMAKE_BUILD_TYPE=RelWithDebInfo
 
 # Perform build
 cmake --build build -- -j$(nproc)
