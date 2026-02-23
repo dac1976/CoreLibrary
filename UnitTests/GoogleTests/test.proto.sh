48734425dc8553319a7df8b8332c0fb686f47097
Duncan Crutchley <drd2k3.coding@tuta.io>
2026-02-23 00:07:14 +0000

    generated updated proto files

--- UnitTests/GoogleTests/test.proto.sh
+++
@@ -1,1 +0,0 @@
-protoc --proto_path=/usr/include --proto_path=/home/duncan/Projects/CoreLibrary/UnitTests/GoogleTests/  --cpp_out=/home/duncan/Projects/CoreLibrary/UnitTests/GoogleTests/ /home/duncan/Projects/CoreLibrary/UnitTests/GoogleTests/test.proto
