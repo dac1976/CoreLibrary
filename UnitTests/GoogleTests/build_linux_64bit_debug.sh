17fb792b593d27b10e2e36238b154344962aad3b
Duncan Crutchley <drd2k3.coding@tuta.io>
2026-02-24 22:54:03 +0000

    Debug build scripts for unit tests

---
+++ UnitTests/GoogleTests/build_linux_64bit_debug.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Make sure you go to /Projvects/ThirdParty/libbacktrace and build and install libbacktrace.
+
+set -e
+
+export CORELIB_ROOT=/home/$USER/Projects/cpp/CoreLibrary
+export CORELIB_BOOST_ROOT=${CORELIB_ROOT}/../ThirdParty/boost_1_90_0
+export CORELIB_BOOST_LIB=${CORELIB_ROOT}/../ThirdParty/boost_1_90_0/lib64-msvc-14.3
+# Not nmeeded fo Linux build
+export CORELIB_BOOST_LIB_NAME_STUB=
+export CORELIB_BOOST_LIB_NAME_STUB_D=
+
+# Tidy previous installation/build folders.
+rm -rf build
+
+# Create build files
+# E.g. CORELIB_DISABLE_ASIO_SERIAL_TESTS=ON, disables ASIO Serial tests, =OFF enables asio serial tests
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug -DCORELIB_USE_STD_FILESYSTEM=ON -DCORELIB_DISABLE_ASIO_SERIAL_TESTS=ON -DCORELIB_DISABLE_ASIO_TESTS=OFF -DCORELIB_DISABLE_CSVGRID_TESTS=OFF -DCORELIB_DISABLE_DEBUGLOG_TESTS=OFF -DCORELIB_DISABLE_FILEUTILS_TESTS=OFF -DCORELIB_DISABLE_FLOAT_COMPARE_TESTS=OFF -DCORELIB_DISABLE_SORTING_TESTS=OFF -DCORELIB_DISABLE_GPROTOBUF_TESTS=ON -DCORELIB_DISABLE_INIFILE_TESTS=OFF -DCORELIB_DISABLE_SERIALIZATION_TESTS=OFF -DCORELIB_DISABLE_STRINGUTILS_TESTS=OFF -DCORELIB_DISABLE_THREADS_TESTS=OFF
+
+# Perform build
+cmake --build build
+
+cd build
+
+ctest
